0853a43f7eee8ddbb17143ded8966fce9668ccc2
André Malveira <www.carlosandre@gmail.com>
2021-10-02 00:42:26 -0300

    update _keys.js

--- src/js/_keys.js
+++ src/js/_keys.js
@@ -1,8 +1,5 @@
-  DIRECTORY_API = (json) => {return `https://andremalveira.github.io/astronaut/api/${json}`}
-  IS_DOCUMENT_MAIN = () => {try {return window.self == window.top;} catch (e) {return false;}}
+
   DOMINIO_NAME_FAKE = 'astronaut.library'
-  STATUS_CONNECTION = true;
-  DOCUMENT_MAIN = IS_DOCUMENT_MAIN();
   LOCALSTORAGE_NAME = 'astronaut_extension_settings';
-  URL_CONNECTION = DIRECTORY_API('connection.json');
+
 
